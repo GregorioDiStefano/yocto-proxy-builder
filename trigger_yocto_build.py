c4fa02f6c3d62247a187e5f786ab5a8ec913afb8
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-10-04 12:03:26 +0200

    updated

--- trigger_yocto_build.py
+++ trigger_yocto_build.py
@@ -16,21 +16,25 @@ job = s.sign(os.environ["JOB"])
 
 r = requests.post('http://home.distefa.no:9999/trigger', data={'job': job, 'machine':machine, 'branch': branch, 'user': user})
 uid = r.json()["build"]
+print "Build: %s started" % uid
 
+start_time = time.time()
+max_time = time.time() * (30 * 60)
 temp = ""
-while True:
+
+while time.time() < max_time:
     try:
-        time.sleep(10)
+        time.sleep(2)
         r = requests.get("http://home.distefa.no:9999/log/" + uid)
         j = json.loads(r.text)
 
         if j != temp:
-            print j
+            print "\n", str(j)
             temp = j
         else:
             print ".",
 
-        if "log" in j and "done" in j["log"] and j["log"]["done"] == "success":
+        if "log" in j and type(j["log"]) is dict and "completed" in j["log"] and j["log"]["completed"]:
             print "done"
             break
 
@@ -38,7 +42,19 @@ while True:
         print "error:", e
         pass
 
-#urllib.urlretrieve ("http://www.example.com/songs/mp3.mp3", "mp3.mp3")
-#urllib.urlretrieve ("http://www.example.com/songs/mp3.mp3", "mp3.mp3")
-#urllib.urlretrieve ("http://www.example.com/songs/mp3.mp3", "mp3.mp3")
-#urllib.urlretrieve ("http://www.example.com/songs/mp3.mp3", "mp3.mp3")
+
+machine = os.environ['YOCTO_MACHINE']
+
+try:
+    os.mkdir(machine)
+except:
+    pass
+
+if machine == "beaglebone":
+    print "Downloading beaglebone output files"
+    urllib.urlretrieve ("https://s3-eu-west-1.amazonaws.com/mender-temp/%s/beaglebone/core-image-base-beaglebone.ext4" % (uid), "beaglebone/core-image-base-beaglebone.ext4")
+    urllib.urlretrieve ("https://s3-eu-west-1.amazonaws.com/mender-temp/%s/beaglebone/core-image-base-beaglebone.sdimg" % (uid), "beaglebone/core-image-base-beaglebone.sdimg")
+elif machine == "vexpress-qemu":
+    print "Downloading vexpress-qemu output files"
+    urllib.urlretrieve ("https://s3-eu-west-1.amazonaws.com/mender-temp/%s/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4" % (uid), "vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4")
+    urllib.urlretrieve ("https://s3-eu-west-1.amazonaws.com/mender-temp/%s/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg" % (uid), "vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg")
